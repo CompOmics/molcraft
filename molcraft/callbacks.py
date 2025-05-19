4881c313e295d8b285c6bc0973af2dc037b2ebf7
Alexander Kensert <alexander.kensert@gmail.com>
2025-05-19 13:23:42 +0200

    add Rollback callback

--- molcraft/callbacks.py
+++ molcraft/callbacks.py
@@ -1,4 +1,6 @@
 import keras
+import warnings
+import numpy as np
 
 
 class TensorBoard(keras.callbacks.TensorBoard):
@@ -31,3 +33,68 @@ class LearningRateDecay(keras.callbacks.LearningRateScheduler):
             return float(lr * keras.ops.exp(-rate))
         
         super().__init__(schedule=lr_schedule, **kwargs)
+
+
+class Rollback(keras.callbacks.Callback):
+    """Rollback callback.
+
+    Currently, this callback simply restores the model and (optionally) the optimizer 
+    variables if current loss deviates too much from the best observed loss.
+
+    This callback might be useful in situations where the loss tend to spike and put 
+    the model in an undesired/problematic high-loss parameter space.
+
+    Args:
+        tolerance (float):
+            The threshold for when the restoration is triggered. The devaiation is 
+            calculated as follows: (current_loss - best_loss) / best_loss.
+    """
+
+    def __init__(
+        self,
+        tolerance: float = 0.5,
+        rollback_optimizer: bool = True,
+    ):
+        super().__init__()
+        self.tolerance = tolerance
+        self.rollback_optimizer = rollback_optimizer
+
+    def on_train_begin(self, logs=None):
+        self._rollback_weights = self._get_model_vars()
+        if self.rollback_optimizer:
+            self._rollback_optimizer_vars = self._get_optimizer_vars()
+        self._rollback_loss = float('inf')
+
+    def on_epoch_end(self, epoch: int, logs: dict = None):
+        current_loss = logs.get('val_loss', logs.get('loss'))
+        deviation = (current_loss - self._rollback_loss) / self._rollback_loss
+
+        if np.isnan(current_loss) or np.isinf(current_loss):
+            self._rollback()
+            # Rolling back model because of nan or inf loss
+            return
+
+        if deviation > self.tolerance:
+            self._rollback()
+            # Rolling back model because of large loss deviation.
+            return
+
+        if current_loss < self._rollback_loss:
+            self._save_state(current_loss)
+
+    def _save_state(self, current_loss: float) -> None:
+        self._rollback_loss = current_loss
+        self._rollback_weights = self._get_model_vars()
+        if self.rollback_optimizer:
+            self._rollback_optimizer_vars = self._get_optimizer_vars()
+
+    def _rollback(self) -> None:
+        self.model.set_weights(self._rollback_weights)
+        if self.rollback_optimizer:
+            self.model.optimizer.set_weights(self._rollback_optimizer_vars)
+
+    def _get_optimizer_vars(self):
+        return [v.numpy() for v in self.model.optimizer.variables]
+
+    def _get_model_vars(self):
+        return self.model.get_weights()
