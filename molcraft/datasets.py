2f35c3e4595bf41eb730a16954b17e1bf63c17eb
Alexander Kensert <alexander.kensert@gmail.com>
2025-10-03 10:44:55 +0200

    update split and add cv_split

--- molcraft/datasets.py
+++ molcraft/datasets.py
@@ -1,123 +1,131 @@
 import numpy as np
 import pandas as pd
+import typing
 
 
 def split(
     data: pd.DataFrame | np.ndarray,
+    *,
     train_size: float | None = None,
     validation_size: float | None = None, 
-    test_size: float = 0.1,
+    test_size: float | None = None,
+    groups: str | np.ndarray = None,
     shuffle: bool = False, 
     random_state: int | None = None,
-) -> pd.DataFrame | np.ndarray:
-    """Splits dataset into subsets.
+) -> tuple[np.ndarray | pd.DataFrame, ...]:
+    """Splits the dataset into subsets.
 
     Args:
         data: 
             A pd.DataFrame or np.ndarray object.
         train_size:
-            Optional train size, as a fraction (`float`) or size (`int`).
+            The size of the train set.
         validation_size:
-            Optional validation size, as a fraction (`float`) or size (`int`).
+            The size of the validation set.
         test_size:
-            Required test size, as a fraction (`float`) or size (`int`).
+            The size of the test set.
+        groups:
+            The groups to perform the splitting on.
         shuffle:
             Whether the dataset should be shuffled prior to splitting.
         random_state:
-            The random state (or seed). Only applicable if shuffling.
+            The random state/seed. Only applicable if shuffling.
     """
+    if not isinstance(data, (pd.DataFrame, np.ndarray)):
+        raise ValueError(f'Unsupported `data` type ({type(data)}).')
 
-    if not isinstance(data, (pd.DataFrame, np.ndarray, list)):
-        raise ValueError(
-            '`data` needs to be a pd.DataFrame, np.ndarray or a list. '
-            f'Found {type(data)}.'
-        )
+    if isinstance(groups, str):
+        groups = data[groups].values 
+    elif groups is None:
+        groups = np.arange(len(data))
     
-    size = len(data)
-    
-    if test_size is None:
-        raise ValueError('`test_size` is required.')
-    elif test_size <= 0:
+    indices = np.unique(groups)
+    size = len(indices)
+
+    if not train_size and not test_size:
         raise ValueError(
-            f'Test size needs to be positive. Found: {test_size}. '
-            'Either specify a positive `float` (fraction) or '
-            'a positive `int` (size).'
+            f'Found both `train_size` and `test_size` to be `None`, '
+            f'specify at least one of them.'
         )
-    if train_size is not None and train_size <= 0:
-        raise ValueError(
-            f'Train size needs to be None or positive. Found: {train_size}. '
-            'Either specify `None`, a positive `float` (fraction) or '
-            'a positive `int` (size).'
-        )
-    if validation_size is not None and validation_size <= 0:
-        raise ValueError(
-            f'Validation size needs to be None or positive. Found: {validation_size}. '
-            'Either specify `None`, a positive `float` (fraction) or '
-            'a positive `int` (size).'
-        )
-    
     if isinstance(test_size, float):
         test_size = int(size * test_size)
-    if validation_size and isinstance(validation_size, float):
+    if isinstance(train_size, float):
+        train_size = int(size * train_size)
+    if isinstance(validation_size, float):
         validation_size = int(size * validation_size)
     elif not validation_size:
         validation_size = 0
 
-    if train_size and isinstance(train_size, float):
-        train_size = int(size * train_size)
-    elif not train_size:
-        train_size = 0
-
     if not train_size:
-        train_size = size - test_size
-        if not validation_size:
-            train_size -= validation_size
-
+        train_size = (size - test_size - validation_size)
+    if not test_size:
+        test_size = (size - train_size - validation_size)
+    
     remainder = size - (train_size + validation_size + test_size)
-
     if remainder < 0:
         raise ValueError(
-            'Sizes of data subsets add up to more than the size of the original data set: '
-            f'{size} < ({train_size} + {validation_size} + {test_size})'
+            f'subset sizes added up to more than the data size.'
         )
-    if test_size <= 0:
-        raise ValueError(
-            f'Test size needs to be greater than 0. Found: {test_size}.'
-        )
-    if train_size <= 0:
-        raise ValueError(
-            f'Train size needs to be greater than 0. Found: {train_size}.'
-        )
-    
     train_size += remainder
 
-    if isinstance(data, pd.DataFrame):
-        if shuffle:
-            data = data.sample(
-                frac=1.0, replace=False, random_state=random_state
-            )
-        train_data = data.iloc[:train_size]
-        test_data = data.iloc[-test_size:]
-        if not validation_size:
-            return train_data, test_data 
-        validation_data = data.iloc[train_size:-test_size]
-        return train_data, validation_data, test_data
-    
-    if not isinstance(data, np.ndarray):
-        data = np.asarray(data)
+    if shuffle:
+        np.random.seed(random_state)
+        np.random.shuffle(indices)
 
-    np.random.seed(random_state)
-
-    random_indices = np.arange(size)
-    np.random.shuffle(random_indices)
-    data = data[random_indices]
-
-    train_data = data[:train_size]
-    test_data = data[-test_size:]
+    train_mask = np.isin(groups, indices[:train_size])
+    test_mask = np.isin(groups, indices[-test_size:])
     if not validation_size:
-        return train_data, test_data 
-    validation_data = data[train_size:-test_size]
-    return train_data, validation_data, test_data
+        return data[train_mask], data[test_mask]
+    validation_mask = np.isin(groups, indices[train_size:-test_size])
+    return data[train_mask], data[validation_mask], data[test_mask]
     
+def cv_split(
+    data: pd.DataFrame | np.ndarray,
+    num_splits: int = 10,
+    groups: str | np.ndarray = None,
+    shuffle: bool = False, 
+    random_state: int | None = None,
+) -> typing.Iterator[
+        tuple[np.ndarray | pd.DataFrame, np.ndarray | pd.DataFrame]
+    ]:
+    """Splits the dataset into cross-validation folds.
 
+    Args:
+        data: 
+            A pd.DataFrame or np.ndarray object.
+        num_splits:
+            The number of cross-validation folds.
+        groups:
+            The groups to perform the splitting on.
+        shuffle:
+            Whether the dataset should be shuffled prior to splitting.
+        random_state:
+            The random state/seed. Only applicable if shuffling.
+    """
+    if not isinstance(data, (pd.DataFrame, np.ndarray)):
+        raise ValueError(f'Unsupported `data` type ({type(data)}).')
     
+    if isinstance(groups, str):
+        groups = data[groups].values
+    elif groups is None:
+        groups = np.arange(len(data))
+
+    indices = np.unique(groups)
+    size = len(indices)
+    
+    if num_splits > size:
+        raise ValueError(
+            f'`num_splits` ({num_splits}) must not be greater than'
+            f'the data size or the number of groups ({size}).'
+        )
+    if shuffle:
+        np.random.seed(random_state)
+        np.random.shuffle(indices)
+
+    indices_splits = np.array_split(indices, num_splits)
+
+    for k in range(num_splits):
+        test_indices = indices_splits[k]
+        test_mask = np.isin(groups, test_indices)
+        train_mask = ~test_mask
+        yield data[train_mask], data[test_mask]
