47ee26b18b39516eb3c4d98561ca4b281e15d718
Alexander Kensert <alexander.kensert@gmail.com>
2026-01-30 14:02:47 +0100

    allow automatic batching of dataset

--- molcraft/models.py
+++ molcraft/models.py
@@ -256,6 +256,8 @@ class GraphModel(layers.GraphLayer, keras.models.Model):
                 x_val = _make_dataset(x[-val_size:], batch_size)
                 x = x[:-val_size]
             x = _make_dataset(x, batch_size, shuffle=kwargs.get('shuffle', True))
+        if isinstance(x, tf.data.Dataset) and tensors.is_scalar(x.element_spec):
+            x = x.batch(batch_size).prefetch(-1)
         return super().fit(x, validation_data=x_val, **kwargs)
     
     def evaluate(self, x: tensors.GraphTensor | tf.data.Dataset, **kwargs):
