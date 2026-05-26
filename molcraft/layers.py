a843b9854ac06915a8444d0fa46056356cd5d7ef
Alexander Kensert <alexander.kensert@gmail.com>
2026-05-26 11:08:36 +0200

    update replicate

--- molcraft/layers.py
+++ molcraft/layers.py
@@ -2413,10 +2413,13 @@ def replicate(
 
     Specifically, this function clones the layer and calls this cloned layer
     with the inputs, to then return its outputs.
+
+    If the original layer was built, its weights will be transferred as well.
     '''
     cloned_layer = layer.__class__.from_config(layer.get_config())
     outputs = cloned_layer(inputs)
-    cloned_layer.set_weights(layer.get_weights())
+    if layer.built:
+        cloned_layer.set_weights(layer.get_weights())
     return outputs
 
 def _serialize_spec(spec: tensors.GraphTensor.Spec) -> dict:
